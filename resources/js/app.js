9430d5de6eb6f71575b92035e5138d620ab35b77
Lutfor Rahman <96254692+lutfor48rahman@users.noreply.github.com>
2023-02-08 15:00:00 +0600

    router add global

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,30 +1,30 @@
-// import "./bootstrap";
-// import { createApp } from "vue";
-// import App from "./App.vue";
-// import router from "./router.js";
+import "./bootstrap";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router.js";
 
-// //import custom user class
-// import User from "./Helpers/User";
+//import custom user class
+import User from "./Helpers/User";
 
-// window.User = User;
+window.User = User;
 
-// //sweet alert toast start
-// import Swal from "sweetalert2";
+//sweet alert toast start
+import Swal from "sweetalert2";
 
-// window.Swal = Swal;
-// // const Swal = require("sweetalert2");
+window.Swal = Swal;
+// const Swal = require("sweetalert2");
 
-// const Toast = Swal.mixin({
-//     toast: true,
-//     position: "top-end",
-//     showConfirmButton: false,
-//     timer: 3000,
-// });
+const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+});
 
-// window.Toast = Toast;
+window.Toast = Toast;
 
-// //sweet alert end
+//sweet alert end
 
-// const app = createApp(App);
-// app.use(router);
-// app.mount("#app");
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
